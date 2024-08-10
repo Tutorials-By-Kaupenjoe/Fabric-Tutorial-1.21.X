9c29ce3d846afec907d8090ef609c35b9b298ee8
Kaupenjoe <N.Kaupenjohann@gmail.com>
2024-08-10 21:43:53 +0200

    custom item groups

---
+++ src/main/java/net/kaupenjoe/tutorialmod/item/ModItemGroups.java
@@ -0,0 +1,36 @@
+package net.kaupenjoe.tutorialmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.kaupenjoe.tutorialmod.TutorialMod;
+import net.kaupenjoe.tutorialmod.block.ModBlocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+    public static final ItemGroup PINK_GARNET_ITEMS_GROUP = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(TutorialMod.MOD_ID, "pink_garnet_items"),
+            FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.PINK_GARNET))
+                    .displayName(Text.translatable("itemgroup.tutorialmod.pink_garnet_items"))
+                    .entries((displayContext, entries) -> {
+                        entries.add(ModItems.PINK_GARNET);
+                        entries.add(ModItems.RAW_PINK_GARNET);
+                    }).build());
+
+    public static final ItemGroup PINK_GARNET_BLOCKS_GROUP = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(TutorialMod.MOD_ID, "pink_garnet_blocks"),
+            FabricItemGroup.builder().icon(() -> new ItemStack(ModBlocks.PINK_GARNET_BLOCK))
+                    .displayName(Text.translatable("itemgroup.tutorialmod.pink_garnet_blocks"))
+                    .entries((displayContext, entries) -> {
+                        entries.add(ModBlocks.PINK_GARNET_BLOCK);
+                        entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
+                    }).build());
+
+
+    public static void registerItemGroups() {
+        TutorialMod.LOGGER.info("Registering Item Groups for " + TutorialMod.MOD_ID);
+    }
+}
