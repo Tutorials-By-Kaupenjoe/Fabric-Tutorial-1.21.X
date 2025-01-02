555614813b0fbd52601022fea11b346fa623e56f
Kaupenjoe <N.Kaupenjohann@gmail.com>
2025-01-02 20:11:43 +0100

    2d texture, 3d model

---
+++ src/main/java/net/kaupenjoe/tutorialmod/mixin/ItemRendererMixin.java
@@ -0,0 +1,54 @@
+package net.kaupenjoe.tutorialmod.mixin;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import net.kaupenjoe.tutorialmod.TutorialMod;
+import net.kaupenjoe.tutorialmod.item.ModItems;
+import net.minecraft.client.render.item.ItemModels;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.render.model.json.ModelTransformationMode;
+import net.minecraft.client.util.ModelIdentifier;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(ItemRenderer.class)
+public abstract class ItemRendererMixin {
+
+    @Shadow
+    @Final
+    private ItemModels models;
+
+    @Shadow
+    public abstract ItemModels getModels();
+
+    @ModifyVariable(
+            method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V",
+            at = @At(value = "HEAD"),
+            argsOnly = true
+    )
+    public BakedModel renderItem(BakedModel bakedModel, @Local(argsOnly = true) ItemStack stack, @Local(argsOnly = true) ModelTransformationMode renderMode) {
+        if (stack.getItem() == ModItems.SPECTRE_STAFF && (renderMode == ModelTransformationMode.GUI || renderMode == ModelTransformationMode.GROUND || renderMode == ModelTransformationMode.FIXED)) {
+            return getModels().getModelManager().getModel(ModelIdentifier.ofInventoryVariant(Identifier.of(TutorialMod.MOD_ID, "spectre_staff")));
+        }
+
+        return bakedModel;
+    }
+
+    @ModifyVariable(
+            method = "getModel",
+            at = @At(value = "STORE"),
+            ordinal = 1
+    )
+    public BakedModel getHeldItemModelMixin(BakedModel bakedModel, @Local(argsOnly = true) ItemStack stack) {
+        if (stack.getItem() == ModItems.SPECTRE_STAFF) {
+            return this.models.getModelManager().getModel(ModelIdentifier.ofInventoryVariant(Identifier.of(TutorialMod.MOD_ID, "spectre_staff_3d")));
+        }
+
+        return bakedModel;
+    }
+}
---
+++ src/main/java/net/kaupenjoe/tutorialmod/mixin/ModelLoaderMixin.java
@@ -0,0 +1,22 @@
+package net.kaupenjoe.tutorialmod.mixin;
+
+import net.kaupenjoe.tutorialmod.TutorialMod;
+import net.minecraft.client.render.model.ModelLoader;
+import net.minecraft.client.util.ModelIdentifier;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ModelLoader.class)
+public abstract class ModelLoaderMixin {
+    @Shadow
+    protected abstract void loadItemModel(ModelIdentifier id);
+
+    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/model/ModelLoader;loadItemModel(Lnet/minecraft/client/util/ModelIdentifier;)V", ordinal = 1))
+    private void onInit(CallbackInfo ci) {
+        this.loadItemModel(ModelIdentifier.ofInventoryVariant(Identifier.of(TutorialMod.MOD_ID, "spectre_staff_3d")));
+    }
+}
